fc5866749e722e301a95c350469a3333e87125e0
Veda Sri <ugs23153_it.durga@cbit.org.in>
2026-08-05 21:32:18 +0530

    Time: 0 ms (100%), Space: 19.9 MB (6.96%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,16 @@
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        old = image[sr][sc]
+        if old == color:
+            return image
+        m, n = len(image), len(image[0])
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or image[r][c] != old:
+                return
+            image[r][c] = color
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        dfs(sr, sc)
+        return image
